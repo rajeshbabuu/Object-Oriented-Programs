f2a95a1ffd4911bbd1340e08c08a0bd03d497e78
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-14 15:38:43 +0530

    Inventory Data Management using JSON File

---
+++ ObjectOrientedPrograms/Model/InventoryModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPrograms.Model
+{
+    public class InventoryModel
+    {
+        public CommonProp Rice { get; set; }
+        public CommonProp Pulses { get; set; }
+        public CommonProp Wheats { get; set; }
+    }
+
+    public class CommonProp
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int PricePerKG { get; set; }
+    }
+}
---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,15 @@
+﻿using ObjectOrientedPrograms.Repository;
+
+namespace ObjectOrientedPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object Oriented Program\n");
+
+            InventoryManager inventoryManager = new InventoryManager();
+            inventoryManager.CalcInventoryValue();
+        }
+    }
+}
